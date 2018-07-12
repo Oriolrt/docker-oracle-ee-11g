a4e373f6e17e0e281ad58d5d0cd01643a2ebb38e
oriolrt <oriolrt@cvc.uab.es>
2018-07-12 15:43:45 +0200

    updated oracle_install.sh

--- install/oracle_install.sh
+++ install/oracle_install.sh
@@ -5,17 +5,32 @@ set -e
 
 cat /etc/security/limits.conf | grep -v oracle | tee /etc/security/limits.conf
 
-echo 'Downloading linux.x64_11gR2_database_1of2.zip'
-file1="linux.x64_11gR2_database_1of2.zip"
-file2="linux.x64_11gR2_database_2of2.zip"
-[ ! -f $file1 ] && wget https://ufile.io/9z7sd   -O $file1
-echo 'Downloading linux.x64_11gR2_database_2of2.zip'
-[ ! -f $file2 ] && wget https://ufile.io/0v6jq  -O $file2
-#cd /install
+Final="https://od.lk/d/MjFfMTIxMjAyMzFf/linux11gR2.zip"
+prefix="https://od.lk/d/MjFfMTIxMjAyMzFf/linux11gR2.z"
+for i in $(seq 1 22)
+do
+	nom="$prefix$( printf '%.2d' $i )";
+    echo "downloading ${nom}"
+    wget $nom 
+done
+wget $Final 
+zip -s 0 linux11gR2.zip --out complete.zip
 echo 'Unzipping'
-unzip -q $file1
-unzip -q $file2
-rm -f linux*.zip
+unzip -q complete.zip
+rm complete.zip
+rm -rf linux11gR2.z*
+
+#echo 'Downloading linux.x64_11gR2_database_1of2.zip'
+#file1="linux.x64_11gR2_database_1of2.zip"
+#file2="linux.x64_11gR2_database_2of2.zip"
+#[ ! -f $file1 ] && wget https://ufile.io/9z7sd   -O $file1
+#echo 'Downloading linux.x64_11gR2_database_2of2.zip'
+#[ ! -f $file2 ] && wget https://ufile.io/0v6jq  -O $file2
+#cd /install
+#echo 'Unzipping'
+#unzip -q $file1
+#unzip -q $file2
+#rm -f linux*.zip
 
 mv database /home/oracle/
 cd /
